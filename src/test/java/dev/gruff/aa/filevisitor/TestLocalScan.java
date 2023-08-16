40b573b00abed1c2423ce85f5c90c0fb29727672
Steve Poole <gruff.wizard@yahoo.com>
2023-08-16 13:37:06 +0100

    fixed self pom discovery test

--- src/test/java/dev/gruff/aa/filevisitor/TestLocalScan.java
+++ src/test/java/dev/gruff/aa/filevisitor/TestLocalScan.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.net.MalformedURLException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestLocalScan {
 
     private static File home=new File(new File(System.getProperty("user.home"),".m2"),"repository");
@@ -13,9 +15,18 @@ public class TestLocalScan {
     public void testfindMyPOM() throws MalformedURLException {
             VisitorBuilder vb=new VisitorBuilder();
             long poms=vb.root("file:"+home.getAbsolutePath())
-                    //.onLeaf(l-> {System.out.println("leaf "+l.toURL().toExternalForm());})
-                    .stream().dropWhile(p -> {return !p.toURL().toExternalForm().endsWith(".pom");}).count();
-            System.out.println(poms);
+                    .selectLeaf( p-> {
+                        String name=p.toURL().toExternalForm();
+                        if(!name.endsWith(".pom")) return false;
+                        String[] parts=name.split("/");
+                       return parts[parts.length-1].startsWith("filevisitor");
+
+                    })
+
+                    .stream().count();
+
+            assertEquals(1,poms);
+
 
     }
 }
